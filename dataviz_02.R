228c24c5ceb76df65f4acce75b05b9f032048235
dc-oksuz <doksuz@ed.ac.uk>
2024-09-24 23:26:34 +0100

    commitadditions

--- dataviz_02.R
+++ dataviz_02.R
@@ -6,7 +6,7 @@ library(rempsyc)
 data <- mtcars
 names(data)[6:3] <- paste0("T", 1:4, "_some-time-variable")
 
-plot_means_over_time(
+p1 <- plot_means_over_time(
   data = data,
   response = names(data)[6:3],
   group = "cyl",
@@ -17,3 +17,21 @@ plot_means_over_time(
   # significance_stars_y: List with structure: list(c("group1", "group2", time))
   significance_stars_y = list(c("4", "8", time = 3),
                               c("4", "8", time = 4)))
+
+p1
+
+p2 <- plot_means_over_time(
+  data = data,
+  response = names(data)[6:3],
+  group = "cyl",
+  groups.order = "decreasing",
+  significance_bars_x = c(3.15, 4.15),
+  significance_stars = c("*", "***"),
+  significance_stars_x = c(3.25, 4.35),
+  significance_stars_y = list(c("4", "8", time = 3),
+                              c("4", "8", time = 4)),
+)
+
+p2 + labs(x = "Time", y = "Mean Response")
+
+
